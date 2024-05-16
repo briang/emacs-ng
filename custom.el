926e9de196746a0bc687b215eb83b762d8656754
brian greenfield <briang@cpan.org>
2024-05-16 21:22:15 +0100

    add `custom.el`
    I don't know if this should be here!

---
+++ custom.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(org-tempo vertico org-modern orderless marginalia ef-themes consult)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:height 180 :family "Iosevka Extended")))))
