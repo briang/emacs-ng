35ff876367cb58ce014073a0faeed9b663b78584
brian greenfield <briang@cpan.org>
2024-05-18 21:22:41 +0100

    custom.el

--- custom.el
+++ custom.el
@@ -4,10 +4,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(org-tempo vertico org-modern orderless marginalia ef-themes consult)))
+   '(ws-butler yasnippet-snippets vertico org-modern orderless marginalia ef-themes consult)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:height 180 :family "Iosevka Extended")))))
+ '(default ((t (:height 160 :family "Iosevka Extended")))))
