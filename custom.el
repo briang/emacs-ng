b5bd33d5e9c50d6efb2625f8a8ace6ac4d62e12d
brian greenfield <briang@cpan.org>
2024-05-22 16:11:25 +0100

    custom.el

--- custom.el
+++ custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(yasnippet-snippets ws-butler vertico org-modern orderless marginalia ef-themes consult)))
+   '(org-mode yasnippet-snippets ws-butler vertico realgud quelpa-use-package perltidy org-modern orderless marginalia flycheck ef-themes consult)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
