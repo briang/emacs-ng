4d63c49fd29e2ef7ee8af35fd5ff086c42553ff2
brian greenfield <briang@cpan.org>
2024-08-13 12:38:35 +0100

    custom.el

--- custom.el
+++ custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(yasnippet-snippets yaml ws-butler which-key vertico treepy taxy-magit-section svg-lib spacious-padding realgud rainbow-delimiters quelpa-use-package perltidy pdf-tools osm org-modern orderless move-text markdown-mode marginalia magit-todos logos keycast jinx helpful flycheck embark-consult ef-themes diminish corfu cdlatex avy auctex all-the-icons-dired)))
+   '(yasnippet-snippets yaml-mode vertico realgud rainbow-delimiters quelpa-use-package perltidy org-modern orderless no-littering move-text marginalia magit-todos helpful flycheck ef-themes doom-modeline consult)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
