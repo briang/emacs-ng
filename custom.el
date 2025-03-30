ef0a9e6c2a12beb2d18149cf7445bd4410784299
brian greenfield <briang@cpan.org>
2025-03-30 11:51:10 +0100

    custom.el

--- custom.el
+++ custom.el
@@ -4,11 +4,12 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(all-the-icons all-the-icons-nerd-fonts centaur-tabs consult doom-modeline
-                   ef-themes flycheck gdscript-mode gdscript-mode.el helpful
-                   magit-todos marginalia move-text no-littering orderless
-                   org-modern perltidy quelpa-use-package rainbow-delimiters
-                   realgud vertico yaml-mode yasnippet-snippets)))
+   '(all-the-icons-nerd-fonts centaur-tabs consult doom-modeline ef-themes flycheck
+                              gdscript-mode gdscript-mode.el helpful magit-todos
+                              marginalia move-text no-littering orderless
+                              org-modern perltidy quelpa-use-package
+                              rainbow-delimiters realgud vertico yaml-mode
+                              yasnippet-snippets)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
