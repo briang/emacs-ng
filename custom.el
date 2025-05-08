8361a2babd8acab3b7c6313f6f3c10bb38439db4
brian greenfield <briang@cpan.org>
2025-05-08 11:32:20 +0100

    custom.el

--- custom.el
+++ custom.el
@@ -3,13 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(all-the-icons-nerd-fonts centaur-tabs consult doom-modeline ef-themes flycheck
-                              gdscript-mode gdscript-mode.el helpful magit-todos
-                              marginalia move-text no-littering orderless
-                              org-modern perltidy quelpa-use-package
-                              rainbow-delimiters realgud vertico yaml-mode
-                              yasnippet-snippets)))
+ '(ignored-local-variable-values '((eval add-hook 'after-save-hook #'org-babel-tangle t t))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
