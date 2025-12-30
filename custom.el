6c461fb0ea98c0ae8ed9ce668bf29013bccc6207
brian greenfield <briang@cpan.org>
2025-12-30 13:27:17 +0000

    custom.el

--- custom.el
+++ custom.el
@@ -3,13 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(all-the-icons-nerd-fonts centaur-tabs consult doom-modeline ef-themes flycheck
-                              gdscript-mode gdscript-mode.el helpful magit-todos
-                              marginalia move-text no-littering orderless
-                              org-modern perltidy quelpa-use-package
-                              rainbow-delimiters realgud vertico yaml-mode
-                              yasnippet-snippets)))
+ '(custom-safe-themes
+   '("d2c76098def8b2b10b45d2092c86ca9c8b95d58fabbc8850d28899181d8f6581" default)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
